a70d22a3d4305b98e671d34ae57f15b998f6b609
slgodwyn <79212430+slgodwyn@users.noreply.github.com>
2021-07-12 09:01:44 -0700

    Update tigervnc.sh

--- src/ubuntu/install/tigervnc.sh
+++ src/ubuntu/install/tigervnc.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
 
 echo "Install TigerVNC server"
-apt install tigervnc-standalone-server tigervnc-xorg-extension tigervnc-viewer
+apt-get install tigervnc-standalone-server tigervnc-xorg-extension tigervnc-viewer
